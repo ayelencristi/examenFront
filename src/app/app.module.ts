d74a134626baa7d4720af96d91a6da560434bc19
ayelencristi <ayelendcristi@gmail.com>
2022-07-20 19:47:56 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WrapperComponent } from './components/layout/wrapper/wrapper.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { MainComponent } from './components/layout/main/main.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WrapperComponent,
+    NavbarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
